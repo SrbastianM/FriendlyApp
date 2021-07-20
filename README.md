ff9ff5c63cae3b26efd4a9f93ad5203637f79a8d
SrbastianM <sebas907.l@gmail.com>
2021-07-20 11:05:35 -0500

    Acutalizacion del README y Bug found la session no se destruye despues de hacer una salida de la pagina

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FriendlyApp
 App for my Academy
 Basicly is a social network, i hope finish faster to show the finally project
+
+# Well is a red social like twiter, in a future this will be a forum like quora or slackOverflow
--- includes/validarLogin.php
+++ includes/validarLogin.php
@@ -4,25 +4,24 @@ include('db.php');
 session_start();
 
 $time = "";
+$UserLogin = "";
+if ($UserLogin = True) {
 
-if (isset($_POST["submitLogin"])) {
-    $user = $_POST["email"];
-    $contraseña = md5($_POST["password"]);
+    if (isset($_POST["submitLogin"])) {
+        $user = $_POST["email"];
+        $contraseña = md5($_POST["password"]);
+        $sql = "SELECT * FROM tbl_users WHERE email ='$user' AND password = '$contraseña'";
+        $result = mysqli_query($conn, $sql);
 
-    $sql = "SELECT * FROM tbl_users WHERE email ='$user' AND password = '$contraseña'";
-    $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            //se para en row y pide que imprima de la primera fila de cada campo
+            header("location:../View/UI.php");
+        } else {
+            header("Location:../login.php");
+            echo "Usuario y/o contraseña no validos";
+        }
 
-    if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        //se para en row y pide que imprima de la primera fila de cada campo
-        header("location:../View/UI.php");
-    } else {
-        header("Location:../login.php");
+        mysqli_close($conn);
     }
-
-    mysqli_close($conn);
 }
-
-
-
-// $User el user que se 
